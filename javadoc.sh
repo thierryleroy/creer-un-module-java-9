cf27de72c2e66f5786e326e41f591e38e5f4e64a
Thierry LEROY <contact@thierryleroy.com>
2020-06-30 18:09:08 +0200

    modification de javadoc.sh et package.sh

--- javadoc.sh
+++ javadoc.sh
@@ -1,3 +1,1 @@
-# javadoc -d javadoc -private --module-source-path source/ --module com.thierryleroy.modulejava.helloworld,com.thierryleroy.modulejava.helloworld.french
-javadoc -d javadoc -private --module-source-path source/ --module com.thierryleroy.modulejava.helloworld
-javadoc -d javadoc -private --module-source-path source/ --module com.thierryleroy.modulejava.helloworld.french
+javadoc -d javadoc -private --module-source-path sources/ --module com.thierryleroy.modulejava.helloworld,com.thierryleroy.modulejava.helloworld.french
--- package.sh
+++ package.sh
@@ -5,8 +5,8 @@ mkdir packages
 fi
 
 #Source
-jar --no-manifest --create  --file packages/com-thierryleroy-modulejava-helloworld-0.1.0-sources.jar -C source/com.thierryleroy.modulejava.helloworld .
-jar --no-manifest --create --file packages/com-thierryleroy-modulejava-helloworld-french-0.1.0-sources.jar -C source/com.thierryleroy.modulejava.helloworld.french .
+jar --no-manifest --create  --file packages/com-thierryleroy-modulejava-helloworld-0.1.0-sources.jar -C sources/com.thierryleroy.modulejava.helloworld .
+jar --no-manifest --create --file packages/com-thierryleroy-modulejava-helloworld-french-0.1.0-sources.jar -C sources/com.thierryleroy.modulejava.helloworld.french .
 #Classes
 jar --create --file packages/com-thierryleroy-modulejava-helloworld-0.1.0.jar -C classes/com.thierryleroy.modulejava.helloworld .
 jar --create --file packages/com-thierryleroy-modulejava-helloworld-french-0.1.0.jar -C classes/com.thierryleroy.modulejava.helloworld.french .
