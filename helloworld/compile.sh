adb35d1b133449a6b2a409931060d9e3890df726
Thierry LEROY <contact@thierryleroy.com>
2020-06-30 14:58:47 +0200

    déplacement des répertoires de source

--- helloworld/compile.sh
+++
@@ -1,5 +0,0 @@
-javac --source-path src --module-version 0.1.0 -d classes \
-./src/com/thierryleroy/modulejava/helloworld/ISayHello.java \
-./src/com/thierryleroy/modulejava/helloworld/Main.java \
-./src/com/thierryleroy/modulejava/helloworld/priv/SayHelloEnglish.java \
-./src/module-info.java
--- helloworld/javadoc.sh
+++
@@ -1,1 +0,0 @@
-javadoc --source-path src  -d javadoc com.thierryleroy.modulejava.helloworld
